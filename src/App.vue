3d3bfd2ec5153dafc45ab3b16435415b2dd78ca4
thomas <yangthomas1992@gmail.com>
2019-05-30 19:17:40 +0800

    ts

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ msg }}</p>
+    <button @click="add">add msg</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {
+  msg = 123;
+
+  add() {
+    this.msg += 1;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
